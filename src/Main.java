c9a2039ce4eee15b21d8bcf8c1f7ead4ac64559c
IceWInd <mr-murzik153246@yandex.ru>
2025-01-17 17:02:24 +0300

    Lines HW end

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ public class Main {
         System.out.println("Задача 3");
         fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "е");
-
         System.out.println("Данные Ф. И. О. сотрудника — "+fullName );
     }
 
