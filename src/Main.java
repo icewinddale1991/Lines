4dc597acd3293f1426073560c24d99bf46963974
IceWInd <mr-murzik153246@yandex.ru>
2025-01-17 19:06:48 +0300

    Lines HW end

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = "Ivanov Ivan Ivanovich";
+        firstName = "Ivan";
+        middleName = "Ivanovich";
+        lastName = "Ivanov";
+        fullname = lastName+ ""+ firstName+"" + middleName;
         System.out.println("Ф. И. О. сотрудника " + fullName);
         System.out.println();
 
