89272287f2a18104ce70d9a8984b6a2cd0d4d024
IceWInd <mr-murzik153246@yandex.ru>
2025-01-17 16:59:54 +0300

    Lines HW end

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф. И. О. сотрудника " + fullName);
+        System.out.println();
+
+        System.out.println("Задача 2");
+        fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " +fullName.toUpperCase());
+        System.out.println();
+
+
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+
+        System.out.println("Данные Ф. И. О. сотрудника — "+fullName );
+    }
+
+
+}
+
